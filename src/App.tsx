efe40b2e7f4384e24a8e70613be7f1c0e61b1d09
Nicolofranceschi <49746918+Nicolofranceschi@users.noreply.github.com>
2023-03-08 14:43:08 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import Drawing from "./components/Drawing";
 import useWindowSize from "./utils/useWindowSize";
 
 export default function App() {
 
-  const { width , height } = useWindowSize();
+  const { width, height } = useWindowSize();
+
+  const [message, setMessage] = useState<MessageEvent | null>(null);
+
+  function displayMessage(evt: MessageEvent) {
+    console.log(evt);
+    setMessage(evt);
+    alert(evt.toString());
+  }
+
+  useEffect(() => {
+    if(!window) return;
+    window.addEventListener("message", displayMessage, false);
+    return window.removeEventListener("message", displayMessage, false);
+  }, [])
 
   return (
-    <Drawing width={ width ?? 0 } height={ height ?? 0 } />
+    <Drawing width={width ?? 0} height={height ?? 0} />
   );
 }
